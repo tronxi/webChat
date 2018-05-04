ca79133d71afcb29cc8eb4ba84761608c1752308
tronxi <tronxi97@gmail.com>
2018-05-05 01:14:40 +0200

    arreglados errores

--- php/entrar.php
+++ php/entrar.php
@@ -13,14 +13,40 @@
       header("Location: ../html/chat.php");
     }
     else {
-      echo "contraseña incorrecta";
-      echo "<a href='../index.html'>Volver</a>";
+      echo <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>WebChat</title>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+          <link href="../css/chat.css" rel="stylesheet">
+        </head>
+        <body class="text-center">
+          <p>
+            contraseña incorrecta
+          </p>
+          <a href='../index.html'>Volver</a>
+        </body>
+      </html>
     }
   }
   else
   {
-    echo "no existe el usuario";
-    echo "<a href='../index.html'>Volver</a>";
+    echo <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>WebChat</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="../css/chat.css" rel="stylesheet">
+      </head>
+      <body class="text-center">
+        <p>
+          no existe el usuario
+        </p>
+        <a href='../index.html'>Volver</a>
+      </body>
+    </html>
   }
   mysqli_close($con);
 ?>

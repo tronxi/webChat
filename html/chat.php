8615d2120b67cf7c94f3f06e8aae902925c5f045
tronxi <tronxi97@gmail.com>
2018-05-04 17:22:12 +0200

    chat

--- html/chat.php
+++ html/chat.php
@@ -8,9 +8,8 @@
     <title>WebChat</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src=""></script>
-    <link href="../css/login.css" rel="stylesheet">
   </head>
-  <body>
+  <body class="text-center">
     <h1>chat</h1>
     <?php
       echo "registrado como ". $_SESSION['usuario'];

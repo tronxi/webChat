44107d4d594913bfc4214e3d0747015ce3570614
tronxi <tronxi97@gmail.com>
2018-05-04 17:32:42 +0200

    cerrar sesion

--- html/chat.php
+++ html/chat.php
@@ -14,7 +14,7 @@
     <?php
       echo "registrado como ". $_SESSION['usuario'];
     ?>
-    <button type="button" class="btn btn-lg btn-primary btn-block" onclick="window.location.href = '../php/cerrarSesion.php'">
+    <button type="button" class="btn btn-lg btn-primary" onclick="window.location.href = '../php/cerrarSesion.php'">
       Cerrar Sesion
     </button>
   </body>

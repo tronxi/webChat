62dbb5a2e1680d7571b1dfea8bcbcc07bd1b2f12
tronxi <tronxi97@gmail.com>
2018-05-04 16:36:10 +0200

    prueba registro

--- php/registro.php
+++ php/registro.php
@@ -4,10 +4,13 @@
 
   $query = "select nombre, contra from usuario where nombre = '".$_POST['usuario']."'";
   $resul = mysqli_query($con, $query);
-  while($fila = mysqli_fetch_array($resul))
+  if($fila = mysqli_fetch_array($resul))
   {
-    echo "usuario ".$_fila['nombre'];
-    echo "\ncontraseña ".$_fila['contra'];
+    echo "el usuario existe";
+  }
+  else
+  {
+    echo "el usuario no existe";
   }
   /*$query = "insert into usuario (nombre, contra) values ('".$_POST['usuario']."','".sha1( $_POST['password'])."')";
   mysqli_query($con, $query);*/

8e4b10cd3ff9e27fb2ff5769fbb8a6974cf9bb58
tronxi <tronxi97@gmail.com>
2018-05-04 23:03:57 +0200

    chat

--- html/chat.php
+++ html/chat.php
@@ -22,5 +22,18 @@
     ?>
     <textarea rows="100" cols="100" class="pre-scrollable form-control"id="aboutDescription" style="resize: none;">
     </textarea>
+<nav class="navbar navbar-inverse navbar-fixed-buttom">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Sitio Web</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Inicio</a></li>
+      <li><a href="#">P&aacute;gina 1</a></li>
+      <li><a href="#">P&aacute;gina 2</a></li>
+      <li><a href="#">P&aacute;gina 3</a></li>
+    </ul>
+  </div>
+</nav>
   </body>
 </html>

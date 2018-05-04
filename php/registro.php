414780a4179c689b693b75e30c643d19e1f6f0c5
tronxi <tronxi97@gmail.com>
2018-05-04 15:51:41 +0200

    errir registro

--- php/registro.php
+++ php/registro.php
@@ -5,5 +5,6 @@
   mysqli_query($con, $query);
   mysqli_close($con);
   header("Location: ../index.html");*/
-  echo "nombre" .$_POST['usuario'] .$_POST['password'];
+  echo $_POST['usuario'];
+  echo $_POST['password'];
 ?>

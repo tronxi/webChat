072f9c5b07f58cd1c94c38d47bd3a967b1a514a5
tronxi <tronxi97@gmail.com>
2018-05-04 16:08:04 +0200

    insertar usuario en tabla usuario

--- php/registro.php
+++ php/registro.php
@@ -1,10 +1,10 @@
 <?php
-  /*$con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
+  $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "pruebaBaseDatos");
-  $query = "insert into tablaPrueba (nombre) values ('".$_POST['nombre']."')";
+  $query = "insert into tablaPrueba (usuario) values ('".$_POST['usuario']."','".sha1( $_POST['password'])."')";
   mysqli_query($con, $query);
   mysqli_close($con);
-  header("Location: ../index.html");*/
+  header("Location: ../index.html");
   echo "usuario ".$_POST['usuario'];
   echo "\ncontraseña ".sha1( $_POST['password']);
 ?>

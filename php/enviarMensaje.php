b875420bfec8d4e3fc7d6f1c228e3535f28334f6
tronxi <tronxi97@gmail.com>
2018-05-05 00:30:26 +0200

    insertar

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -1,8 +1,9 @@
 <?php
-$con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
-mysqli_select_db($con, "chat");
+  session_start();
+  $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
+  mysqli_select_db($con, "chat");
 
-$query = "insert into mensaje (nombre, texto, fecha) values ('".$_SESSION['usuario']."', '".$_REQUEST['mensaje']."', '25-5-5')";
-mysqli_query($con, $query);
-mysqli_close($con);
+  $query = "insert into mensaje (nombre, texto, fecha) values ('".$_SESSION['usuario']."', '".$_REQUEST['mensaje']."', '25-5-5')";
+  mysqli_query($con, $query);
+  mysqli_close($con);
 ?>

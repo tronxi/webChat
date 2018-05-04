9358fea482af6e7b4b8d1a6087e2091a868b9b0a
tronxi <tronxi97@gmail.com>
2018-05-04 15:45:09 +0200

    registro php

---
+++ php/registro.php
@@ -0,0 +1,9 @@
+<?php
+  /*$con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
+  mysqli_select_db($con, "pruebaBaseDatos");
+  $query = "insert into tablaPrueba (nombre) values ('".$_POST['nombre']."')";
+  mysqli_query($con, $query);
+  mysqli_close($con);
+  header("Location: ../index.html");*/
+  echo "nombre" ".$_POST['usuario']." ".$_POST['contraseña']."
+?>

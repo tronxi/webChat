5cb7f0a9f6d9551a8dd5ec3e537061ac881be2a3
tronxi <tronxi97@gmail.com>
2018-05-04 16:25:00 +0200

    insertar

--- php/registro.php
+++ php/registro.php
@@ -1,9 +1,15 @@
 <?php
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "chat");
-  $query = "insert into usuario (nombre, contra) values ('".$_POST['usuario']."','".sha1( $_POST['password'])."')";
+
+  $query = "select nombre, contra from usuario where nombre = '".$_POST['usuario']."'";
+  $resul = mysqli_query($con, $query);
+  $fila = $fila = mysqli_fetch_array($resul)
+  echo "usuario ".$_fila['nombre'];
+  echo "\ncontraseña ".$_fila['contra']);
+  /*$query = "insert into usuario (nombre, contra) values ('".$_POST['usuario']."','".sha1( $_POST['password'])."')";
   mysqli_query($con, $query);
-  mysqli_close($con);
+  mysqli_close($con);*/
   echo "usuario ".$_POST['usuario'];
   echo "\ncontraseña ".sha1( $_POST['password']);
 ?>

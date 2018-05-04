d82698dde6848ac06281c57b44d34722e7a8a1b9
tronxi <tronxi97@gmail.com>
2018-05-05 01:21:22 +0200

    arreglados errors php

--- php/entrar.php
+++ php/entrar.php
@@ -18,7 +18,7 @@
         <head>
           <meta charset="utf-8">
           <title>WebChat</title>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
           <link href="../css/chat.css" rel="stylesheet">
         </head>
         <body class="text-center">

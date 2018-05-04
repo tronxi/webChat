89b6d20b61864b198b243151ab91918181661a9e
tronxi <tronxi97@gmail.com>
2018-05-04 15:48:38 +0200

    error registro

--- php/registro.php
+++ php/registro.php
@@ -5,5 +5,5 @@
   mysqli_query($con, $query);
   mysqli_close($con);
   header("Location: ../index.html");*/
-  echo "nombre" ".$_POST['usuario']." ".$_POST['contraseña']."
+  echo "nombre" .$_POST['usuario'] .$_POST['contraseña'];
 ?>

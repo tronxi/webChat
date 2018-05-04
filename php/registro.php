28e3b02643cc74d1f138bc613be16bcd63b142a0
tronxi <tronxi97@gmail.com>
2018-05-05 01:26:02 +0200

    arreglados errores html

--- php/registro.php
+++ php/registro.php
@@ -6,8 +6,21 @@
   $resul = mysqli_query($con, $query);
   if($fila = mysqli_fetch_array($resul))
   {
-    echo "<p>El usuario ya existe</p>";
-    echo "<a href='../index.html'>Volver</a>";
+    echo "<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset='utf-8'>
+        <title>WebChat</title>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
+        <link href='../css/chat.css' rel='stylesheet'>
+      </head>
+      <body class='text-center'>
+        <p>
+          Ya existe el usuario
+        </p>
+        <a href='../index.html'>Volver</a>
+      </body>
+    </html>";
   }
   else
   {

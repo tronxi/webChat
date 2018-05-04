c025c21747e707669b9cffc5b019b909c74da7b9
tronxi <tronxi97@gmail.com>
2018-05-04 16:10:52 +0200

    registro

--- php/registro.php
+++ php/registro.php
@@ -1,7 +1,7 @@
 <?php
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
-  mysqli_select_db($con, "pruebaBaseDatos");
-  $query = "insert into tablaPrueba (usuario) values ('".$_POST['usuario']."','".sha1( $_POST['password'])."')";
+  mysqli_select_db($con, "chat");
+  $query = "insert into usuario (nombre, contra) values ('".$_POST['usuario']."','".sha1( $_POST['password'])."')";
   mysqli_query($con, $query);
   mysqli_close($con);
   header("Location: ../index.html");

12adfca7c9623dc926f9b10ed6c870a1fd4e8eea
tronxi <tronxi97@gmail.com>
2018-05-06 20:31:58 +0200

    set charset

--- php/mostrarMensaje.php
+++ php/mostrarMensaje.php
@@ -2,7 +2,7 @@
   header('Content-Type: text/html; charset=utf-8');
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "chat");
-
+  mysqli_set_charset($conexion,'utf8');  
   $query = "select nombre, texto, fecha from mensaje";
   $resul = mysqli_query($con, $query);
   while($fila = mysqli_fetch_array($resul))

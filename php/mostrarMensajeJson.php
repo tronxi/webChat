484ceb1ee12bf87646ee52ec0fd3c755c0aeb072
tronxi <tronxi97@gmail.com>
2018-06-10 17:54:31 +0200

    prueba json

--- php/mostrarMensajeJson.php
+++ php/mostrarMensajeJson.php
@@ -2,7 +2,7 @@
   header('Content-Type: text/html; charset=utf-8');
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "chat");
-  $query = "select nombre from mensaje";
+  $query = "select nombre, texto, fecha from mensaje";
   $resul = mysqli_query($con, $query);
   $objJson = array();
   while($fila = mysqli_fetch_array($resul))

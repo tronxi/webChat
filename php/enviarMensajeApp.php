e7a03eb307f6c703dcf11e708658237be7225684
tronxi <tronxi97@gmail.com>
2018-06-11 19:20:31 +0200

    enviarMensajeApp

---
+++ php/enviarMensajeApp.php
@@ -0,0 +1,9 @@
+<?php
+  session_start();
+  $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
+  mysqli_select_db($con, "chat");
+
+  $query = "insert into mensaje (nombre, texto, fecha) values ('".$_POST['usuario']."', '".$_POST['mensaje']."', '".date('Y/m/d H:i:s')."')";
+  mysqli_query($con, $query);
+  mysqli_close($con);
+?>

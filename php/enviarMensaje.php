ce09bef006c2f7432a93d6c244c63e8e96156c68
tronxi <tronxi97@gmail.com>
2018-06-16 20:24:24 +0200

    cambio post enviarMensaje

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -3,7 +3,7 @@
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "chat");
 
-  $query = "insert into mensaje (nombre, texto, fecha) values ('".$_SESSION['usuario']."', '".$_REQUEST['mensaje']."', '".date('Y/m/d H:i:s')."')";
+  $query = "insert into mensaje (nombre, texto, fecha) values ('".$_SESSION['usuario']."', '".$_POST['mensaje']."', '".date('Y/m/d H:i:s')."')";
   mysqli_query($con, $query);
   mysqli_close($con);
 ?>

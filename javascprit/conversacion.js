fc818832c0e3b6f6ccd2e263c603c56104c8a0dc
tronxi <tronxi97@gmail.com>
2018-06-17 19:00:05 +0200

    mostrar conversaciones

--- javascprit/conversacion.js
+++ javascprit/conversacion.js
@@ -26,12 +26,21 @@ function procesarEventos()
 	if(conexion.readyState == 4)
 	{
 		var conversacionJson = JSON.parse(conexion.responseText);
+		var conversacionHTML = "";
 		for(var i in conversacionJson)
 		{
-			console.log(conversacionJson[i].nombre);
+			conversacionHTML += "<div class='card-deck mb-3 text-center'>
+			<div class='card mb-4 box-shadow'>
+				<div class='card-header'>
+					<h4 class="my-0 font-weight-normal">conversacionJson[i].nombre</h4>
+				</div>
+				<div class='card-body'>
+					<button type='button' class='btn btn-lg btn-block btn-outline-primary'>hablar</button>
+				</div>
+			</div>
+		</div>";
 		}
-		conversaciones.innerHTML = conversacionJson.nombre;
-		console.log("hola");
+		conversaciones.innerHTML = conversacionHTML;
 	}
 	else
 	{

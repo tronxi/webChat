d6088b665a7ac7f654f19c28f424d0a76f8f473d
tronxi <tronxi97@gmail.com>
2018-06-17 22:19:28 +0200

    debug conversacion

--- php/abrirConversacion.php
+++ php/abrirConversacion.php
@@ -1,12 +1,13 @@
 <?php
+session_start();
 	$con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   	mysqli_select_db($con, "chat");
 
-  $query = "insert into usuario (nombre, contra) values ('mecagoentodo','".sha1( 'mecagoentodo')."')";
+  $query = "insert into usuario (nombre, contra) values ('mecagoentododosveces','".sha1( 'mecagoentodo')."')";
   $resul = mysqli_query($con, $query);
   mysqli_close($con);
 
-	/*session_start();
+	/*
     $_SESSION['conversacion'] = $_POST['conversacion'];
 	$cont = file_get_contents("../html/chat.php"); 
 	echo $cont;*/

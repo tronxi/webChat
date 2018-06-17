ea11dd4a45e7dd3aaacefa45da7119faf3162c20
tronxi <tronxi97@gmail.com>
2018-06-17 19:01:44 +0200

    mostrar conversaciones

--- javascprit/conversacion.js
+++ javascprit/conversacion.js
@@ -29,16 +29,7 @@ function procesarEventos()
 		var conversacionHTML = "";
 		for(var i in conversacionJson)
 		{
-			conversacionHTML += "<div class='card-deck mb-3 text-center'>
-			<div class='card mb-4 box-shadow'>
-				<div class='card-header'>
-					<h4 class="my-0 font-weight-normal">conversacionJson[i].nombre</h4>
-				</div>
-				<div class='card-body'>
-					<button type='button' class='btn btn-lg btn-block btn-outline-primary'>hablar</button>
-				</div>
-			</div>
-		</div>";
+			conversacionHTML += "<div class='card-deck mb-3 text-center'><div class='card mb-4 box-shadow'><div class='card-header'><h4 class='my-0 font-weight-normal'>conversacionJson[i].nombre</h4></div><div class='card-body'><button type='button' class='btn btn-lg btn-block btn-outline-primary'>hablar</button></div></div></div>";
 		}
 		conversaciones.innerHTML = conversacionHTML;
 	}

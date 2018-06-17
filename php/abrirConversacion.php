2a26e1ee7c0cb95bab0db0930cccdf4d32656089
tronxi <tronxi97@gmail.com>
2018-06-17 22:12:41 +0200

    debug conversacion

--- php/abrirConversacion.php
+++ php/abrirConversacion.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
-    $_SESSION['conversacion'] = $_POST['conversacion'];
+	//session_start();
+    //$_SESSION['conversacion'] = $_POST['conversacion'];
     header("Location: ../html/chat.php");
 ?>

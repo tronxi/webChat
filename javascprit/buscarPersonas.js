3a1eeeba62d93168b28c8d5395c22e3c95afb0c0
tronxi <tronxi97@gmail.com>
2018-06-18 01:18:39 +0200

    error url

--- javascprit/buscarPersonas.js
+++ javascprit/buscarPersonas.js
@@ -23,10 +23,10 @@ function abrirConversacion(nombre)
 function procesarEventos()
 {
   var busqueda = document.getElementById('personas');
-  var busquedaHTML = "";
-  var busquedaJson = JSON.parse(conexion1.responseText);
+  var busquedaHTML = ""; 
   if(conexion.readyState == 4)
   {
+	var busquedaJson = JSON.parse(conexion1.responseText);
     for(var i in busquedaJson)
 	{
 		busquedaHTML += "<div class='card mb-4 box-shadow'><div class='card-header'><h4 class='my-0 font-weight-normal'>" + busquedaJson[i].nombre + "</h4></div><div class='card-body'><button type='button' class='btn btn-lg btn-block btn-outline-primary' id=" + busquedaJson[i].nombre + " onclick='abrirConversacion("+busquedaJson[i].nombre+")'>hablar</button></div></div>";

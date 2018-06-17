0224684d33874d5dcb8d135ffb2694125ddf46d8
tronxi <tronxi97@gmail.com>
2018-06-17 22:56:11 +0200

    debug conversacion

--- php/abrirConversacion.php
+++ php/abrirConversacion.php
@@ -3,4 +3,9 @@
 	$_SESSION['conversacion'] = $_POST['conversacion'];
 	console.log("hola");
 	header("Location: ../html/chat.php");
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
 ?>

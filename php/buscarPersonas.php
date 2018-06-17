3145ae8d38262990183527e348fe6a5c3654e282
tronxi <tronxi97@gmail.com>
2018-06-18 01:23:06 +0200

    error url

--- php/buscarPersonas.php
+++ php/buscarPersonas.php
@@ -3,7 +3,7 @@
   header('Content-Type: text/html; charset=utf-8');
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "chat");
-  $query = "select nombre from usuario where nombre !=' ".$_SESSION['usuario']."'";
+  $query = "select nombre from usuario where nombre !='".$_SESSION['usuario']."'";
   $resul = mysqli_query($con, $query);
   $objJson = array();
   while($fila = mysqli_fetch_array($resul))

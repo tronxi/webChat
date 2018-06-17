3185574040f0bd8a7bc8f752c69a22a4fa427b1d
tronxi <tronxi97@gmail.com>
2018-06-18 00:44:40 +0200

    ordenar conversaciones por fecha

--- php/buscarConversacion.php
+++ php/buscarConversacion.php
@@ -4,19 +4,22 @@
   mysqli_select_db($con, "chat");
 
   $query = "SELECT DISTINCT
-    nombre, id_conversacion
+    c.nombre, c.id_conversacion, MAX(fecha) as ultimaFecha
 FROM
     conversacion c
+        LEFT JOIN
+    mensaje m ON c.id_conversacion = m.id_conversacion
 WHERE
-    id_conversacion != 0
+    c.id_conversacion != 0
         AND c.nombre != '".$_SESSION['usuario']."'
-        AND id_conversacion IN (SELECT
+        AND c.id_conversacion IN (SELECT 
             c.id_conversacion
         FROM
             conversacion c
         WHERE
             c.nombre = '".$_SESSION['usuario']."')
-ORDER BY c.id_conversacion";
+GROUP BY c.id_conversacion
+ORDER BY ultimaFecha DESC;";
 
   $resul = mysqli_query($con, $query);
   $objJson = array();

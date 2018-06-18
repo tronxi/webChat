7aa54d61b723c905e7b65dd837133d1533dfb8c1
tronxi <tronxi97@gmail.com>
2018-06-19 00:13:01 +0200

    crear notificacion

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -6,7 +6,16 @@
 	$query = "insert into mensaje (nombre, texto, fecha, id_conversacion) values ('".$_SESSION['usuario']."', '".$_POST['mensaje']."', '".date('Y/m/d H:i:s')."', ".$_SESSION['conversacion'].")";
 	mysqli_query($con, $query);
 
-	$query = "insert into conversacion (estado) values(1) where id_conversacion = ".$_SESSION['conversacion']." and nombre in (select nombre from conversacion where id_conversacion = ".$_SESSION['conversacion'].") and nombre != '".$_SESSION['usuario']."'";
+	$query = "UPDATE conversacion
+			SET
+				estado = 1
+			WHERE
+				id_conversacion = ".$_SESSION['conversacion']."
+					AND nombre = (SELECT
+						nombre
+					WHERE
+						id_conversacion = ".$_SESSION['conversacion']."
+							AND nombre != '".$_SESSION['usuario']."');";
 	mysqli_query($con, $query);
 	mysqli_close($con);
 ?>

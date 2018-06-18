a0645009c006e3034c08f4a9980557c88d067e97
tronxi <tronxi97@gmail.com>
2018-06-18 14:27:40 +0200

    cambios interfaz

--- html/conversacion.php
+++ html/conversacion.php
@@ -6,6 +6,7 @@
 	<title>WebChat</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="../javascprit/conversacion.js"></script>
+	<link href="../css/conversacion.css" rel="stylesheet">
 </head>
 
 <body class="text-center">

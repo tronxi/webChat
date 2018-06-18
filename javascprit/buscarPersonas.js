b174b7c5c7f964273d6ec1973e74dcb4b40b4b01
tronxi <tronxi97@gmail.com>
2018-06-18 13:39:05 +0200

    arreglador errores

--- javascprit/buscarPersonas.js
+++ javascprit/buscarPersonas.js
@@ -47,14 +47,8 @@ function procesarEventos()
 		var busquedaJson = JSON.parse(conexion1.responseText);
 		for(var i in busquedaJson)
 		{
-			//busquedaJson[i].nombre = htmlEntities(busquedaJson[i].nombre);
 			busquedaHTML += "<div class='card mb-4 box-shadow'><div class='card-header'><h4 class='my-0 font-weight-normal'>" + busquedaJson[i].nombre + "</h4></div><div class='card-body'><button type='button' class='btn btn-lg btn-block btn-outline-primary' id=" + busquedaJson[i].nombre + " onclick='abrirConversacion(" + busquedaJson[i].nombre + ")'>hablar</button></div></div>";
 		}
 		personas.innerHTML = busquedaHTML;
 	}
-}
-
-function htmlEntities(str)
-{
-	return String(str).replace(/&/g, ' ').replace(/</g, ' ').replace(/>/g, ' ').replace(/"/g, ' ').replace(/\//g,' ').replace(" ","_");
 }

6e974fa91f5a9eebd39a7ff556fe44ff5b79f4a3
tronxi <tronxi97@gmail.com>
2018-06-18 19:30:42 +0200

    cambio mostrar mensajes

--- javascprit/mostrarMensaje.js
+++ javascprit/mostrarMensaje.js
@@ -1,36 +1,39 @@
 addEventListener('load', inicializarEventos, false);
 
 function inicializarEventos()
+{}
+var actualizar = setInterval(function ()
 {
-
-}
-var actualizar = setInterval(function()
-{
-  mostrarDatos()
+	mostrarDatos()
 }, 500);
 
 function mostrarDatos()
 {
-  var url = "../php/mostrarMensaje.php";
-  mostrar(url);
+	var url = "../php/mostrarMensajeJson.php";
+	mostrar(url);
 }
-
 var conexion1;
 
 function mostrar(url)
 {
-  conexion1 = new XMLHttpRequest(url);
-  conexion1.onreadystatechange = escribir;
-  conexion1.open("GET", url, true);
-  conexion1.send();
+	conexion1 = new XMLHttpRequest(url);
+	conexion1.onreadystatechange = escribir;
+	conexion1.open("GET", url, true);
+	conexion1.send();
 }
 
 function escribir()
 {
-  var conversacion = document.getElementById('conversacion');
-  if (conexion1.readyState == 4)
-  {
-    conversacion.value = conexion1.responseText;
-    conversacion.scrollTop = conversacion.scrollHeight;
-  }
+	var conversacion = document.getElementById('conversacion');
+	if(conexion1.readyState == 4)
+	{
+		var mensajesJson = JSON.parse(conexion1.responseText);
+		var mensajes = "";
+		for(var i in mensajesJson)
+		{
+			mensajes += mensajesJson[i].nombre + "- " + mensajesJson[i].fecha + ": " + mensajesJson[i].texto + "\n";
+		}
+		conversacion.value = conexion1.mensajes;
+		conversacion.scrollTop = conversacion.scrollHeight;
+	}
 }

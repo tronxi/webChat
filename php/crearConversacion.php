e1a838c41390cf8e7bc3521442d5ed06a9b0fa27
tronxi <tronxi97@gmail.com>
2018-06-18 12:39:26 +0200

    cambio consulta

--- php/crearConversacion.php
+++ php/crearConversacion.php
@@ -6,11 +6,9 @@
 	$con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
 	mysqli_select_db($con, "chat");
 	$query = "SELECT DISTINCT
-    c.nombre, c.id_conversacion, MAX(fecha) as ultimaFecha
+    c.nombre
 FROM
     conversacion c
-        LEFT JOIN
-    mensaje m ON c.id_conversacion = m.id_conversacion
 WHERE
     c.id_conversacion != 0
         AND c.nombre != '".$_SESSION['usuario']."'
@@ -19,9 +17,7 @@ WHERE
         FROM
             conversacion c
         WHERE
-            c.nombre = '".$_SESSION['usuario']."')
-GROUP BY c.id_conversacion
-ORDER BY ultimaFecha DESC;";
+            c.nombre = '".$_SESSION['usuario']."');";
 	$resul = mysqli_query($con, $query);
 
 	$objJson = array();

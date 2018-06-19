d61c3dab2b6d162bcea81bb227293f508dd38ac1
tronxi <tronxi97@gmail.com>
2018-06-20 00:05:29 +0200

    cifrar mensajes

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -3,7 +3,9 @@
 	include 'datos.php';
 	$con = mysqli_connect($host, $usuario, $contraseña); mysqli_select_db($con, $bd);
 
-	$query = "insert into mensaje (nombre, texto, fecha, id_conversacion) values ('".$_SESSION['usuario']."', '".$_POST['mensaje']."', '".date('Y/m/d H:i:s')."', ".$_SESSION['conversacion'].")";
+	$mensajeCifrado = encrypt_decrypt('encrypt', $_POST['mensaje']);
+
+	$query = "insert into mensaje (nombre, texto, fecha, id_conversacion) values ('".$_SESSION['usuario']."', '".$mensajeCifrado."', '".date('Y/m/d H:i:s')."', ".$_SESSION['conversacion'].")";
 	mysqli_query($con, $query);
 
 	$query = "UPDATE conversacion
@@ -14,4 +16,29 @@
 					AND nombre = '".$_SESSION['usuario']."'";
 	mysqli_query($con, $query);
 	mysqli_close($con);
+
+	function encrypt_decrypt($action, $string)
+	{
+		$output = false;
+
+		$encrypt_method = "AES-256-CBC";
+		$secret_key = 'This is my secret key';
+		$secret_iv = 'This is my secret iv';
+
+		$key = hash('sha256', $secret_key);
+
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+		if( $action == 'encrypt' )
+		{
+			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+			$output = base64_encode($output);
+		}
+		else if( $action == 'decrypt' )
+		{
+			$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+
+		return $output;
+	}
 ?>

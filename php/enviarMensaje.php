cf7b22254d2c354ba25881edc7508f53e87c53f1
tronxi <tronxi97@gmail.com>
2018-06-20 00:21:05 +0200

    contrasñas seguras

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -22,8 +22,6 @@
 		$output = false;
 
 		$encrypt_method = "AES-256-CBC";
-		$secret_key = 'This is my secret key';
-		$secret_iv = 'This is my secret iv';
 
 		$key = hash('sha256', $secret_key);
 

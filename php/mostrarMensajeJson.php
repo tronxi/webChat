b9c1cb5c849a99094e27d1a6088236d340c41dcf
tronxi <tronxi97@gmail.com>
2018-06-20 00:07:49 +0200

    descirfar mensajes

--- php/mostrarMensajeJson.php
+++ php/mostrarMensajeJson.php
@@ -12,7 +12,7 @@
   {
     $objJson[] = array('nombre' => $fila['nombre'],
                         'fecha' => $fila['fecha'],
-                      'texto' => $fila['texto']);
+                      'texto' => encrypt_decrypt('decrypt', $fila['texto']));
   }
 
   $query = "UPDATE conversacion
@@ -29,4 +29,29 @@
   mysqli_close($con);
 
   echo json_encode($objJson);
+
+    function encrypt_decrypt($action, $string)
+	{
+		$output = false;
+
+		$encrypt_method = "AES-256-CBC";
+		$secret_key = 'This is my secret key';
+		$secret_iv = 'This is my secret iv';
+
+		$key = hash('sha256', $secret_key);
+
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+		if( $action == 'encrypt' )
+		{
+			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+			$output = base64_encode($output);
+		}
+		else if( $action == 'decrypt' )
+		{
+			$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+
+		return $output;
+	}
 ?>

0eba781024d2c1abc60122541fa63ce9b95fc619
tronxi <tronxi97@gmail.com>
2018-06-20 23:29:03 +0200

    prueba php

--- php/buscarConversacionApp.php
+++ php/buscarConversacionApp.php
@@ -12,13 +12,13 @@ FROM
     mensaje m ON c.id_conversacion = m.id_conversacion
 WHERE
     c.id_conversacion != 0
-        AND c.nombre != '".$_POST['usuario']."'
+        AND c.nombre != 'tronxi'
         AND c.id_conversacion IN (SELECT
             c.id_conversacion
         FROM
             conversacion c
         WHERE
-            c.nombre = '".$_POST['usuario']."')
+            c.nombre = 'tronxi')
 GROUP BY c.id_conversacion
 ORDER BY ultimaFecha DESC;";
 

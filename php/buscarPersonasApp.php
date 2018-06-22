def7f2f3e10253ea928ed3d764b7597356686986
tronxi <tronxi97@gmail.com>
2018-06-22 23:18:45 +0200

    buscar personas app

---
+++ php/buscarPersonasApp.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+  header('Content-Type: text/html; charset=utf-8');
+  include 'datos.php';
+  $con = mysqli_connect($host, $usuario, $contraseña);
+  mysqli_select_db($con, $bd);
+
+  $query = "select nombre from usuario where nombre !='".$_POST['usuario']."'";
+  $resul = mysqli_query($con, $query);
+  $objJson = array();
+  while($fila = mysqli_fetch_array($resul))
+  {
+    $objJson[] = array('nombre' => $fila['nombre']);
+  }
+  mysqli_close($con);
+
+  echo json_encode($objJson);
+?>

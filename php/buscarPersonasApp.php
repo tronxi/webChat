078271db33f5215a2439306ac0cba998294e63db
tronxi <tronxi97@gmail.com>
2018-06-23 01:56:17 +0200

    array buscar personas app

--- php/buscarPersonasApp.php
+++ php/buscarPersonasApp.php
@@ -5,12 +5,14 @@
   $con = mysqli_connect($host, $usuario, $contraseña);
   mysqli_select_db($con, $bd);
 
-  $query = "select nombre from usuario where nombre !='".$_POST['usuario']."'";
+  $query = "select nombre, id_conversacion, estado from usuario where nombre !='".$_POST['usuario']."'";
   $resul = mysqli_query($con, $query);
   $objJson = array();
   while($fila = mysqli_fetch_array($resul))
   {
-    $objJson[] = array('nombre' => $fila['nombre']);
+    $objJson[] = array('nombre' => $fila['nombre'],
+					  'idConversacion' => $fila['id_conversacion'],
+					  'estado' => $fila['estado']);
   }
   mysqli_close($con);
 

69e889506444ddde9d5d16ecea1963f3242a8910
sergio garcia sanchez <sergio.gsanchez@alumnos.upm.es>
2018-11-15 16:48:05 +0100

    prueba envio notificaciones

--- php/enviarMensajeApp.php
+++ php/enviarMensajeApp.php
@@ -15,6 +15,15 @@
 				id_conversacion = ".$_POST['conversacion']."
 					AND nombre = '".$_POST['usuario']."'";
 	mysqli_query($con, $query);
+
+	$query = "SELECT u.nombre as nombre, u.token as token from usuario u, conversacion c where u.nombre = c.nombre and c.id_conversacion = ".$_POST['conversacion']." and u.nombre != '".$_POST['usuario']."';";
+	mysqli_query($con, $query);
+	$tokenNecesario = "";
+	while($fila = mysqli_fetch_array($resul))
+  	{
+		$tokenNecesario = $fila['token'];
+	}
+	enviar($tokenNecesario);
 	mysqli_close($con);
 
 	function encrypt_decrypt($action, $string)
@@ -38,5 +47,44 @@
 		}
 
 		return $output;
+	}
+
+	function enviar($tokenNecs)
+	{
+		define('API_ACCESS_KEY','AAAA4iUUfRc:APA91bFSDG8l61UvTH3y3keSmNfTodFgaH9rNj2IE84z3Ob9YDtZqLkuGFNEv0G3kZnsj_8XYo5I0CCtQ9ZR9ZX1YIgtu01o7ePDcyU8lQuD6W6X-enuPL85zJsFnDTWXB5O61irybXO');
+		$fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+		$token= $tokenNecs;
+	
+			$notification = [
+				'title' =>$_POST['usuario']."'",
+				'body' =>  $_POST['mensaje']
+			];
+			$extraNotificationData = ["message" => $notification,"moredata" =>'dd'];
+	
+			$fcmNotification = [
+				//'registration_ids' => $tokenList, //multple token array
+				'to'        => $token, //single token
+				'notification' => $notification,
+				'data' => $extraNotificationData
+			];
+	
+			$headers = [
+				'Authorization: key=' . API_ACCESS_KEY,
+				'Content-Type: application/json'
+			];
+	
+	
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+			$result = curl_exec($ch);
+			curl_close($ch);
+	
+	
+			echo $result;
 	}
 ?>

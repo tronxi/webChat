e5ba622fa046bc29a782db95dfa63a114d049f34
sergio garcia sanchez <sergio.gsanchez@alumnos.upm.es>
2018-11-15 14:08:38 +0100

    cambios envio notificaciones

--- php/enviarNotificacion.php
+++ php/enviarNotificacion.php
@@ -1,20 +1,38 @@
 <?php
     
-    use sngrl\PhpFirebaseCloudMessaging\Client;
-    use sngrl\PhpFirebaseCloudMessaging\Message;
-    use sngrl\PhpFirebaseCloudMessaging\Recipient\Device;
-    use sngrl\PhpFirebaseCloudMessaging\Notification;
-
-    $server_key = 'AAAA4iUUfRc:APA91bFSDG8l61UvTH3y3keSmNfTodFgaH9rNj2IE84z3Ob9YDtZqLkuGFNEv0G3kZnsj_8XYo5I0CCtQ9ZR9ZX1YIgtu01o7ePDcyU8lQuD6W6X-enuPL85zJsFnDTWXB5O61irybXO';
-    $client = new Client();
-    $client->setApiKey($server_key);
-
-    $message = new Message();
-    $message->setPriority('high');
-    $message->addRecipient(new Device('fM_EIbnlLkI:APA91bFDYErasu18pKFh6JXZCJ8m9uW5vEyjuJf-Geen02AWpKIlyzIaM55GbolC6IWCjuRJDj7NSwMYt_w9KqtJ48hIXkpCSu72FdMDWEGDvSRUKJXd8gCYh8lOEhcsa9mg8Gbo69Wg'));
-    $message->setNotification(new Notification('php', 'soy una puta maquina'));
-
-    $response = $client->send($message);
-    var_dump($response->getStatusCode());
-    var_dump($response->getBody()->getContents());
+    define('API_ACCESS_KEY','AAAA4iUUfRc:APA91bFSDG8l61UvTH3y3keSmNfTodFgaH9rNj2IE84z3Ob9YDtZqLkuGFNEv0G3kZnsj_8XYo5I0CCtQ9ZR9ZX1YIgtu01o7ePDcyU8lQuD6W6X-enuPL85zJsFnDTWXB5O61irybXO');
+    $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+    $token='fM_EIbnlLkI:APA91bFDYErasu18pKFh6JXZCJ8m9uW5vEyjuJf-Geen02AWpKIlyzIaM55GbolC6IWCjuRJDj7NSwMYt_w9KqtJ48hIXkpCSu72FdMDWEGDvSRUKJXd8gCYh8lOEhcsa9mg8Gbo69Wg';
+   
+        $notification = [
+               'title' =>'title',
+               'body' => 'body of message.'
+           ];
+           $extraNotificationData = ["message" => $notification,"moredata" =>'dd'];
+   
+           $fcmNotification = [
+               //'registration_ids' => $tokenList, //multple token array
+               'to'        => $token, //single token
+               'notification' => $notification,
+               'data' => $extraNotificationData
+           ];
+   
+           $headers = [
+               'Authorization: key=' . API_ACCESS_KEY,
+               'Content-Type: application/json'
+           ];
+   
+   
+           $ch = curl_init();
+           curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+           curl_setopt($ch, CURLOPT_POST, true);
+           curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+           curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+           $result = curl_exec($ch);
+           curl_close($ch);
+   
+   
+           echo $result;
 ?>

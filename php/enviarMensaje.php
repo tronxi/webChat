346aa88277d609d75ce8145ab8c9c8a18e399ee3
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 07:33:32 +0100

    original

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -1,11 +1,9 @@
 <?php
 	session_start();
 	include 'datos.php';
-
 	$con = mysqli_connect($host, $usuario, $contraseña); mysqli_select_db($con, $bd);
-	$this->load->library("Crypto");
-	$mensajeCifrado = $this->crypto->encrypt( $_POST['mensaje'], 'd6F3Efeq');
-	//$mensajeCifrado = encrypt_decrypt('encrypt', $_POST['mensaje']);
+
+	$mensajeCifrado = encrypt_decrypt('encrypt', $_POST['mensaje']);
 
 	$query = "insert into mensaje (nombre, texto, fecha, id_conversacion) values ('".$_SESSION['usuario']."', '".$mensajeCifrado."', '".date('Y/m/d H:i:s')."', ".$_SESSION['conversacion'].")";
 	mysqli_query($con, $query);
@@ -28,7 +26,6 @@
 	enviar($tokenNecesario);
 	mysqli_close($con);
 
-	
 	function encrypt_decrypt($action, $string)
 	{
 		$output = false;
@@ -95,99 +92,4 @@
 	
 			echo $result;
 	}
-
-	class Crypto
-{
-   const METHOD = 'aes-256-ctr';
-
-   public function encrypt($plaintext, $password, $salt='', $encode = false)
-   {
-      $keyAndIV = self::evpKDF($password, $salt);
-
-      $ciphertext = openssl_encrypt(
-                                       $plaintext,
-                                       self::METHOD,
-                                       $keyAndIV["key"],
-                                       OPENSSL_RAW_DATA,
-                                       $keyAndIV["iv"]
-                                    );
-
-      $ciphertext = bin2hex($ciphertext);
-
-      if ($encode)
-      {
-         $ciphertext = base64_encode($ciphertext);
-      }
-
-      return $ciphertext;
-   }
-
-
-   public function decrypt($ciphertext, $password, $salt='', $encoded = false)
-   {
-      if ( $encoded )
-      {
-         $ciphertext = base64_decode($ciphertext, true);
-
-         if ($ciphertext === false)
-         {
-            throw new Exception('Encryption failure');
-         }
-      }
-
-      $ciphertext = hex2bin($ciphertext);
-      $keyAndIV   = self::evpKDF($password, $salt);
-
-      $plaintext = openssl_decrypt(
-                                       $ciphertext,
-                                       self::METHOD,
-                                       $keyAndIV["key"],
-                                       OPENSSL_RAW_DATA,
-                                       $keyAndIV["iv"]
-                                    );
-
-      return $plaintext;
-   }
-
-   public function evpKDF($password, $salt, $keySize = 8, $ivSize = 4, $iterations = 1, $hashAlgorithm = "md5")
-   {
-      $targetKeySize = $keySize + $ivSize;
-      $derivedBytes  = "";
-
-      $numberOfDerivedWords = 0;
-      $block         = NULL;
-      $hasher        = hash_init($hashAlgorithm);
-
-      while ($numberOfDerivedWords < $targetKeySize)
-      {
-         if ($block != NULL)
-         {
-            hash_update($hasher, $block);
-         }
-
-         hash_update($hasher, $password);
-         hash_update($hasher, $salt);
-
-         $block   = hash_final($hasher, TRUE);
-         $hasher  = hash_init($hashAlgorithm);
-
-         // Iterations
-         for ($i = 1; $i < $iterations; $i++)
-         {
-            hash_update($hasher, $block);
-            $block   = hash_final($hasher, TRUE);
-            $hasher  = hash_init($hashAlgorithm);
-         }
-
-         $derivedBytes .= substr($block, 0, min(strlen($block), ($targetKeySize - $numberOfDerivedWords) * 4));
-
-         $numberOfDerivedWords += strlen($block)/4;
-      }
-
-      return array(
-                     "key" => substr($derivedBytes, 0, $keySize * 4),
-                     "iv"  => substr($derivedBytes, $keySize * 4, $ivSize * 4)
-                   );
-   }
-}
 ?>

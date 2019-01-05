2dd27ad385fb38a2a68158f7b1e4546c623fb521
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 06:08:28 +0100

    encrypt

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -26,16 +26,13 @@
 	function encrypt($message)
 	{
 		$password = $secret_key;
-		if (OPENSSL_VERSION_NUMBER <= 268443727) {
-			throw new RuntimeException('OpenSSL Version too old, vulnerability to Heartbleed');
-		}
 		
 		$iv_size        = openssl_cipher_iv_length(AES_METHOD);
 		$iv             = openssl_random_pseudo_bytes($iv_size);
 		$ciphertext     = openssl_encrypt($message, AES_METHOD, $password, OPENSSL_RAW_DATA, $iv);
 		$ciphertext_hex = bin2hex($ciphertext);
 		$iv_hex         = bin2hex($iv);
-		return $iv_hex:$ciphertext_hex;
+		return "$iv_hex:$ciphertext_hex";
 	}
 
 	enviar($tokenNecesario);

9b0d57c296e32c816d7f1698871777a5be03ede4
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 07:20:05 +0100

    encriptado

--- php/enviarMensaje.php
+++ php/enviarMensaje.php
@@ -1,9 +1,11 @@
 <?php
 	session_start();
 	include 'datos.php';
+	const METHOD = 'aes-256-ctr';
+
 	$con = mysqli_connect($host, $usuario, $contraseña); mysqli_select_db($con, $bd);
 
-	$mensajeCifrado = encrypt( $_POST['mensaje']);
+	$mensajeCifrado = encrypt( $_POST['mensaje'], 'd6F3Efeq');
 	//$mensajeCifrado = encrypt_decrypt('encrypt', $_POST['mensaje']);
 
 	$query = "insert into mensaje (nombre, texto, fecha, id_conversacion) values ('".$_SESSION['usuario']."', '".$mensajeCifrado."', '".date('Y/m/d H:i:s')."', ".$_SESSION['conversacion'].")";
@@ -27,20 +29,68 @@
 	enviar($tokenNecesario);
 	mysqli_close($con);
 
-	function encrypt($mensaje)
-	{
-		$cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-		/*$key256 = '12345678901234561234567890123456';
-		$iv = '1234567890123456';
-		$plainText = $mensaje;
-		mcrypt_generic_init($cipher, $key256, $iv);
-		$cipherText256 = mcrypt_generic($cipher,$plainText );
-		mcrypt_generic_deinit($cipher);
-		$cipherHexText256 =bin2hex($cipherText256);
-		//return $cipherHexText256;*/
-		return "hola";
-	}
+	public function encrypt($plaintext, $password, $salt='', $encode = false)
+   {
+      $keyAndIV = self::evpKDF($password, $salt);
 
+      $ciphertext = openssl_encrypt(
+                                       $plaintext,
+                                       self::METHOD,
+                                       $keyAndIV["key"],
+                                       OPENSSL_RAW_DATA,
+                                       $keyAndIV["iv"]
+                                    );
+
+      $ciphertext = bin2hex($ciphertext);
+
+      if ($encode)
+      {
+         $ciphertext = base64_encode($ciphertext);
+      }
+
+      return $ciphertext;
+   }
+
+	public function evpKDF($password, $salt, $keySize = 8, $ivSize = 4, $iterations = 1, $hashAlgorithm = "md5")
+   {
+      $targetKeySize = $keySize + $ivSize;
+      $derivedBytes  = "";
+
+      $numberOfDerivedWords = 0;
+      $block         = NULL;
+      $hasher        = hash_init($hashAlgorithm);
+
+      while ($numberOfDerivedWords < $targetKeySize)
+      {
+         if ($block != NULL)
+         {
+            hash_update($hasher, $block);
+         }
+
+         hash_update($hasher, $password);
+         hash_update($hasher, $salt);
+
+         $block   = hash_final($hasher, TRUE);
+         $hasher  = hash_init($hashAlgorithm);
+
+         // Iterations
+         for ($i = 1; $i < $iterations; $i++)
+         {
+            hash_update($hasher, $block);
+            $block   = hash_final($hasher, TRUE);
+            $hasher  = hash_init($hashAlgorithm);
+         }
+
+         $derivedBytes .= substr($block, 0, min(strlen($block), ($targetKeySize - $numberOfDerivedWords) * 4));
+
+         $numberOfDerivedWords += strlen($block)/4;
+      }
+
+      return array(
+                     "key" => substr($derivedBytes, 0, $keySize * 4),
+                     "iv"  => substr($derivedBytes, $keySize * 4, $ivSize * 4)
+                   );
+   }
 	function encrypt_decrypt($action, $string)
 	{
 		$output = false;

877be62061ab109bc5fedcac54697249b5e44d6b
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 13:04:55 +0100

    mostrar mensaje

--- php/mostrarMensajeJson.php
+++ php/mostrarMensajeJson.php
@@ -36,9 +36,8 @@
 
 		$encrypt_method = "AES-256-CBC";
 
-		$key = hash('sha256', $secret_key);
-
-		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+		$key = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+    	$iv = 'AAAAAAAAAAAAAAAA';
 
 		if( $action == 'encrypt' )
 		{
